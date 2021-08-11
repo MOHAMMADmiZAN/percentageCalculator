04e092a5837f2d845c5752e4724b0b2fd7987f48
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2021-08-12 03:30:39 +0600

    percentageCalculator update

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let result = document.querySelector('.percentageResult')
+let firstValue = document.querySelector('#firstValue')
+
+let percent = document.querySelector('#Percentage')
+
+let submit = document.querySelector('#percentageButton')
+submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (firstValue.value === '' || percent.value === '') {
+        alert('input all filled')
+    } else {
+        let x = Number.parseFloat(firstValue.value)
+        let y = Number.parseFloat(percent.value)
+        x = x / 100
+        result.innerHTML = `${x * y}`
+    }
+
+})
+
+
+
+
